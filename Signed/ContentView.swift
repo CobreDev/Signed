cbc40c2669c287fb4c0a60b8aada5d4cc3802c00
mac-user669 <cooper@cooperhull.com>
2020-06-09 11:43:23 -0400

    Initial Commit

---
+++ Signed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Signed
+//
+//  Created by Cooper Hull on 6/9/20.
+//  Copyright © 2020 mac-user669. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
